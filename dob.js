77f04957396c9bc2e4fb3b1bff2eeb7f04be4387
abhinavrajput1111 <abhinav.r.1111@gmail.com>
2024-04-14 10:28:02 +0530

    changes in HTML file

--- dob.js
+++ dob.js
@@ -7,21 +7,19 @@ const output = document.querySelector("#output");
 const heading = document.querySelector("#heading-Age");
 
 // heading.style.cssText = "color:red;";
-output.style.cssText = "color: red; font-size: 2rem; "
-wrapper.style.cssText = "background: white; margin-top: 5rem; padding-top:0.5rem; width: 50%; margin-left:auto; margin-right: auto; height: 50vh"
+output.style.cssText = "color: red; font-size: 2rem; ";
+wrapper.style.cssText =
+  "background: white; margin-top: 5rem; padding-top:0.5rem; width: 50%; margin-left:auto; margin-right: auto; height: 50vh";
 
-btn.addEventListener("click", ()=>{
-  const dob = input.value.slice(0,4);
- 
-  if(dob == ""){
+btn.addEventListener("click", () => {
+  const dob = input.value.slice(0, 4);
+
+  if (dob == "") {
     alert("Enter Your DOB first");
-  }
-
-  else{
+  } else {
     operations = 2024 - Number(dob);
     let para = document.createElement("p");
     para.innerText = `Your Age is ${operations} old`;
-  output.innerText = para.innerText;  
+    output.innerText = para.innerText;
   }
-
-})
+});
